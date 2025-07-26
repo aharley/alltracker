1a0cf412657e5e43e2a68fa03898f2000c250750
aharley <harley.adam@gmail.com>
2025-07-26 14:00:50 -0700

    added some test utils

--- utils/data.py
+++ utils/data.py
@@ -3,6 +3,80 @@ import dataclasses
 import torch.nn.functional as F
 from dataclasses import dataclass
 from typing import Any, Optional, Dict
+import utils.misc
+import numpy as np
+
+def replace_invalid_xys_with_nearest(xys, valids):
+    # replace invalid xys with nearby ones
+    invalid_idx = np.where(valids==0)[0]
+    valid_idx = np.where(valids==1)[0]
+    for idx in invalid_idx:
+        nearest = valid_idx[np.argmin(np.abs(valid_idx - idx))]
+        xys[idx] = xys[nearest]
+    return xys
+
+def standardize_test_data(rgbs, trajs, visibs, valids, S_cap=600, only_first=False, seq_len=None):
+    trajs = trajs.astype(np.float32) # S,N,2
+    visibs = visibs.astype(np.float32) # S,N
+    valids = valids.astype(np.float32) # S,N
+
+    # only take tracks that make sense
+    visval_ok = np.sum(valids*visibs, axis=0) > 1
+    trajs = trajs[:,visval_ok]
+    visibs = visibs[:,visval_ok]
+    valids = valids[:,visval_ok]
+
+    # fill in missing data (for visualization)
+    N = trajs.shape[1]
+    for ni in range(N):
+        trajs[:,ni] = replace_invalid_xys_with_nearest(trajs[:,ni], valids[:,ni])
+
+    # use S_cap or seq_len (legacy)
+    if seq_len is not None:
+        S = min(len(rgbs), seq_len)
+    else:
+        S = len(rgbs)
+    S = min(S, S_cap)
+
+    if only_first:
+        # we'll find the best frame to start on
+        best_count = 0
+        best_ind = 0
+
+        for si in range(0,len(rgbs)-64):
+            # try this slice
+            visibs_ = visibs[si:min(si+S,len(rgbs)+1)] # S,N
+            valids_ = valids[si:min(si+S,len(rgbs)+1)] # S,N
+            visval_ok0 = (visibs_[0]*valids_[0]) > 0 # N
+            visval_okA = np.sum(visibs_*valids_, axis=0) > 1 # N
+            all_ok = visval_ok0 & visval_okA
+            # print('- slicing %d to %d; sum(ok) %d' % (si, min(si+S,len(rgbs)+1), np.sum(all_ok)))
+            if np.sum(all_ok) > best_count:
+                best_count = np.sum(all_ok)
+                best_ind = si
+        si = best_ind
+        rgbs = rgbs[si:si+S]
+        trajs = trajs[si:si+S]
+        visibs = visibs[si:si+S]
+        valids = valids[si:si+S]
+        vis_ok0 = visibs[0] > 0  # N
+        trajs = trajs[:,vis_ok0]
+        visibs = visibs[:,vis_ok0]
+        valids = valids[:,vis_ok0]
+        # print('- best_count', best_count, 'best_ind', best_ind)
+
+    if seq_len is not None:
+        rgbs = rgbs[:seq_len]
+        trajs = trajs[:seq_len]
+        valids = valids[:seq_len]
+    
+    # req two timesteps valid (after seqlen trim)
+    visval_ok = np.sum(visibs*valids, axis=0) > 1
+    trajs = trajs[:,visval_ok]
+    valids = valids[:,visval_ok]
+    visibs = visibs[:,visval_ok]
+
+    return rgbs, trajs, visibs, valids
 
 
 @dataclass(eq=False)
